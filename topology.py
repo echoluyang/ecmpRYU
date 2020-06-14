f4dc1efbc4f28e425195d95b107577b08e361454
echoluyang <55120898+echoluyang@users.noreply.github.com>
2020-06-13 23:17:46 -0400

    Add files via upload

---
+++ topology.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSController
+from mininet.node import CPULimitedHost, Host, Node
+from mininet.node import OVSKernelSwitch, UserSwitch
+from mininet.node import IVSSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.link import TCLink, Intf
+from subprocess import call
+
+def myNetwork():
+
+    net = Mininet( topo=None,
+                   build=False,
+                   ipBase='10.0.0.0/8')
+
+    info( '*** Adding controller\n' )
+    c0=net.addController(name='c0',
+                      controller=RemoteController,
+                      ip='127.0.0.1',
+                      protocol='tcp',
+                      port=6633)
+
+    info( '*** Add switches\n')
+    s5 = net.addSwitch('s5', cls=OVSKernelSwitch)
+    s2 = net.addSwitch('s2', cls=OVSKernelSwitch)
+    s4 = net.addSwitch('s4', cls=OVSKernelSwitch)
+    s7 = net.addSwitch('s7', cls=OVSKernelSwitch)
+    s8 = net.addSwitch('s8', cls=OVSKernelSwitch)
+    s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
+    s3 = net.addSwitch('s3', cls=OVSKernelSwitch)
+    s6 = net.addSwitch('s6', cls=OVSKernelSwitch)
+
+    info( '*** Add hosts\n')
+    h2 = net.addHost('h2', cls=Host, ip='10.0.0.2', defaultRoute=None)
+    h1 = net.addHost('h1', cls=Host, ip='10.0.0.1', defaultRoute=None)
+
+    info( '*** Add links\n')
+    net.addLink(h1, s1)
+    net.addLink(s1, s2)
+    net.addLink(s2, s3)
+    net.addLink(s3, s8)
+    net.addLink(s5, s8)
+    net.addLink(s4, s5)
+    net.addLink(s1, s4)
+    net.addLink(s1, s6)
+    net.addLink(s6, s7)
+    net.addLink(s7, s8)
+    net.addLink(s6, s5)
+    net.addLink(s8, h2)
+
+    info( '*** Starting network\n')
+    net.build()
+    info( '*** Starting controllers\n')
+    for controller in net.controllers:
+        controller.start()
+
+    info( '*** Starting switches\n')
+    net.get('s5').start([c0])
+    net.get('s2').start([c0])
+    net.get('s4').start([c0])
+    net.get('s7').start([c0])
+    net.get('s8').start([c0])
+    net.get('s1').start([c0])
+    net.get('s3').start([c0])
+    net.get('s6').start([c0])
+
+    info( '*** Post configure switches and hosts\n')
+
+    CLI(net)
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    myNetwork()
+
